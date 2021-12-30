8af78a50bd5240abe248d443de38fb7621eca339
Scott Everett <burningcupidxxx@gmail.com>
2021-12-30 11:47:55 -0800

    Create conditionals that generate a password according to user specified input

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ var noLow = letUp.concat(nums, specChar);
 var noUp = letLow.concat(nums, specChar);
 var noNum = letLow.concat(letUp, specChar);
 var noSpecChar = letLow.concat(letUp, nums);
+var noUpNum = letLow.concat(specChar);
+var noLet = nums.concat(specChar);
+var noLowNum = letUp.concat(specChar);
+var upNum = letUp.concat(nums);
+var lowNum = letLow.concat(nums);
+var lowUp = letLow.concat(letUp);
 
 
 
@@ -16,12 +22,12 @@ var noSpecChar = letLow.concat(letUp, nums);
 // Write password to the #password input
 function writePassword() {
 
-var inputLength = prompt("Please choose a password length between 8 and 128 characters");
+  var inputLength = prompt("Please choose a password length between 8 and 128 characters");
+
 
   while (inputLength < 8 || inputLength > 128 || isNaN(inputLength) == true) {
-    if (inputLength < 8 || inputLength > 128 || isNaN(inputLength) == true)  {
+    
       var inputLength = prompt("Please enter a valid number between 8 and 128 characters");
-    }
   }
       var inputLowerCase = confirm("Click OK to include lowercase letters,\nCancel to exclude them");
       var inputUpperCase = confirm("Click OK to include uppercase letters,\nCancel to exclude them");
@@ -40,7 +46,7 @@ var inputLength = prompt("Please choose a password length between 8 and 128 char
     }
   }
 
-// User confirms all character types
+  // User confirms all character types
   if (inputLowerCase === true && inputUpperCase === true && inputNumeric === true && inputSpecial === true) {
     function generatePassword(length) {
       var result = "";
@@ -50,7 +56,6 @@ var inputLength = prompt("Please choose a password length between 8 and 128 char
       return result;
     }
     document.querySelector("#password").innerHTML = generatePassword(inputLength);
-    console.log("hey");
   }
   
 
@@ -59,26 +64,179 @@ var inputLength = prompt("Please choose a password length between 8 and 128 char
     function generatePassword(length) {
       var result = "";
       for (i=0; i<length; i++) {
-        result += allChar.charAt(Math.floor(Math.random() * allChar.length));
+        result += noLow.charAt(Math.floor(Math.random() * noLow.length));
       }
       return result;
     }
     document.querySelector("#password").innerHTML = generatePassword(inputLength);
-    console.log("hey");
   }
 
 
-  // User excludes numbers
-  if (inputLowerCase === true && inputUpperCase === true && inputNumeric === false && inputSpecial === true) {
+  // User excludes upper case
+  if (inputLowerCase === true && inputUpperCase === false && inputNumeric === true && inputSpecial === true) {
     function generatePassword(length) {
       var result = "";
       for (i=0; i<length; i++) {
-        result += allChar.charAt(Math.floor(Math.random() * allChar.length));
+        result += noUp.charAt(Math.floor(Math.random() * noUp.length));
       }
       return result;
     }
     document.querySelector("#password").innerHTML = generatePassword(inputLength);
-    console.log("hey");
+  }
+
+
+   // User excludes numbers
+   if (inputLowerCase === true && inputUpperCase === true && inputNumeric === false && inputSpecial === true) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += noNum.charAt(Math.floor(Math.random() * noNum.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+   // User excludes special characters
+   if (inputLowerCase === true && inputUpperCase === true && inputNumeric === false && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += noSpecChar.charAt(Math.floor(Math.random() * noSpecChar.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes upper case and numbers
+   if (inputLowerCase === true && inputUpperCase === false && inputNumeric === false && inputSpecial === true) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += noUpNum.charAt(Math.floor(Math.random() * noUpNum.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes all letter characters
+   if (inputLowerCase === false && inputUpperCase === false && inputNumeric === true && inputSpecial === true) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += noLet.charAt(Math.floor(Math.random() * no.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes lower case and numbers
+   if (inputLowerCase === false && inputUpperCase === true && inputNumeric === false && inputSpecial === true) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += noLowNum.charAt(Math.floor(Math.random() * noLowNum.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes lower case and special characters
+   if (inputLowerCase === false && inputUpperCase === true && inputNumeric === true && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += upNum.charAt(Math.floor(Math.random() * upNum.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes upper case and special characters
+   if (inputLowerCase === true && inputUpperCase === false && inputNumeric === true && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += lowNum.charAt(Math.floor(Math.random() * lowNum.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+   // User excludes numbers and special characters
+   if (inputLowerCase === true && inputUpperCase === true && inputNumeric === false && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += lowUp.charAt(Math.floor(Math.random() * lowUp.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+  // User confirms only lower case
+  if (inputLowerCase === true && inputUpperCase === false && inputNumeric === false && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += letLow.charAt(Math.floor(Math.random() * letLow.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+  // User confirms only upper case
+  if (inputLowerCase === false && inputUpperCase === true && inputNumeric === false && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += letUp.charAt(Math.floor(Math.random() * letUp.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+  // User confirms only numbers
+  if (inputLowerCase === false && inputUpperCase === false && inputNumeric === true && inputSpecial === false) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += nums.charAt(Math.floor(Math.random() * nums.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
+  }
+
+
+  // User confirms only special characters
+  if (inputLowerCase === false && inputUpperCase === false && inputNumeric === false && inputSpecial === true) {
+    function generatePassword(length) {
+      var result = "";
+      for (i=0; i<length; i++) {
+        result += specChar.charAt(Math.floor(Math.random() * specChar.length));
+      }
+      return result;
+    }
+    document.querySelector("#password").innerHTML = generatePassword(inputLength);
   }
 
   
